6c7bb73fc37cd55c07226c71c33a2b83fd8f0347
Glitch (glitch-hello-node) <none>
2024-08-14 02:56:42 +0000

    ❄️🕹 Checkpoint ./public/STecSenai-localStorage.js:38290350/16688

--- public/STecSenai-localStorage.js
+++ public/STecSenai-localStorage.js
@@ -1,6 +1,6 @@
 // STecSenai-localStorage.js
 
-/* ******************** OPÇÕES DE DADOS DA STRING dados ****
+/* ******************** OPÇÕES DE DADOS DO LOCAL STORAGE PARA FUNçÂO getItem ****
     
     nome,                estadoPf          estadoPj          cnpjPj              testemunhaSenai,
     cpf,                 cep               cepPj             razaoPj
@@ -11,6 +11,84 @@
    
 ********************************************************* */
 
+document.addEventListener("DOMContentLoaded", function () {
+  const cnpjPj = localStorage.getItem("cnpjPj");
+  const razaoPj = localStorage.getItem("razaoPj");
+  const fantasiaPj = localStorage.getItem("fantasiaPj");
+  const nome = localStorage.getItem("nome");
+  const logradouroPf = localStorage.getItem("logradouroPf");
+  const nomeCliente = localStorage.getItem("nomeCliente");
+  const numeroResidencia = localStorage.getItem("numeroResidencia");
+  const telefone = localStorage.getItem("telefone");
+  const emailpessoal = localStorage.getItem("email");
+  const cpf = localStorage.getItem("cpf");
+  const servicos = localStorage.getItem("servico");
+  
+
+  // Função para obter o nome fantasia -------------------------------------------------
+  function obterNomeFantasia() {
+    // Coleta o valor de dadosCnpj.fantasia
+    let nomeFantasia = fantasiaPj;
+    
+    // Verifica se o nomeFantasia é vazio ou nulo e ajusta o valor
+    if (!nomeFantasia || nomeFantasia.trim() === "") {
+      nomeFantasia = ", nome fantasia não atribuído";
+    } else {
+      nomeFantasia = ", nome fantasia " + fantasiaPj;
+    }
+    
+    return nomeFantasia;
+  }
+
+  // Obtém o nome fantasia
+  const nomeFantasia = obterNomeFantasia();
+// -------------------------------------------------------------------------------------
+   
+   
+  if (!dadosCnpj || !cepDigitado) {
+    alert(
+      "Verifique se o CNPJ ou o CEP são apenas números e se estão corretos. Por favor, volte e preencha os dados novamente."
+    );
+    window.location.href = "/index.html";
+    return;
+  }
+
+  const reportDiv = document.getElementById("report");
+  reportDiv.innerHTML = `
+    <p style="text-align: justify;">
+      ${dadosCnpj.nome}${nomeFantasia}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero},
+      bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${dadosCnpj.telefone},
+      e-mail ${dadosCnpj.email}, empresa neste ato representada por ${nomeCliente}, brasileiro(a), Empresário(a)/Autônomo(a) com 
+      inscrição no CPF nº ${cpf}, residente à ${cepDigitado.logradouro}, nº ${numeroResidencia}, bairro ${cepDigitado.bairro},
+      CEP ${cepDigitado.cep}, na comarca de ${cepDigitado.localidade} - ${cepDigitado.uf}, telefone de contato ${telefone} e e-mail
+      pessoal ${emailpessoal}, denominado(a) como <b>CONTRATANTE</b>
+    </p>`;
+
+  const reportProduto = document.getElementById("reportProduto");
+  reportProduto.innerHTML = `
+    <p style="text-align: justify;">
+    Produto específico da prestação dos serviços: ${servicos}
+    </p>`;
+  
+  const clienteAssinante = document.getElementById("clienteAssinante");
+  clienteAssinante.innerHTML = `
+    <h3 style="text-align: justify;">
+    <b>CONTRATANTE / EMPRESA</b><br>
+    <b>${nomeCliente}<b><br>
+    <b>${cpf}<b>
+    </p>`;
+
+
+  
+  document.getElementById("voltar").addEventListener("click", function () {
+    window.location.href = "/index.html";
+  });
+});
+
+
+
+
+/* /************************************************************************
 // Função para gerar o texto do contrato com base nos dados do localStorage
 function gerarTextoContrato() {
     const dados = obterDadosContrato();
@@ -88,3 +166,4 @@ function testemunhaDoContratoSenai() {
     }
 }
 export { testemunhaDoContratoSenai }
+*/
