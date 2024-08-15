4f6119db8750863e9d5dc1efdd30f55ecdfb66dd
Glitch (backup-gerador-contratos-stec) <none>
2024-08-15 00:45:47 +0000

    Rewound to commit 297dc2978811304a3d4936de4eb88d1fbbda95a3 + 0 edits

--- public/STecSenai-localStorage.js
+++ public/STecSenai-localStorage.js
@@ -8,41 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const emailpessoal = localStorage.getItem("email");
   const cpf = localStorage.getItem("cpf");
   const servicos = localStorage.getItem("servico");
-  
-  // Função para obter o nome fantasia
-  function obterNomeFantasia() {
-    // Coleta o valor de dadosCnpj.fantasia
-    let nomeFantasia = dadosCnpj.fantasia;
-    
-    // Verifica se o nomeFantasia é vazio ou nulo e ajusta o valor
-    if (!nomeFantasia || nomeFantasia.trim() === "") {
-      nomeFantasia = ", nome fantasia não atribuído";
-    } else {
-      nomeFantasia = ", nome fantasia " + dadosCnpj.fantasia;
-    }
-    
-    return nomeFantasia;
-  }
+  const fantasiaPj = localStorage.getItem("fantasiaPj");
+  const emailPj = localStorage.getItem("emailPj");
+  const telefonePj = localStorage.getItem("telefonePj");
 
-  // Obtém o nome fantasia
-  const nomeFantasia = obterNomeFantasia();
-
-   
-   
-  if (!dadosCnpj || !cepDigitado) {
-    alert(
-      "Verifique se o CNPJ ou o CEP são apenas números e se estão corretos. Por favor, volte e preencha os dados novamente."
-    );
-    window.location.href = "/index.html";
-    return;
-  }
 
   const reportDiv = document.getElementById("report");
   reportDiv.innerHTML = `
     <p style="text-align: justify;">
-      ${dadosCnpj.nome}${nomeFantasia}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero},
-      bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${dadosCnpj.telefone},
-      e-mail ${dadosCnpj.email}, empresa neste ato representada por ${nomeCliente}, brasileiro(a), Empresário(a)/Autônomo(a) com 
+      ${dadosCnpj.nome}, nome fantasia ${fantasiaPj}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero},
+      bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${telefonePj},
+      e-mail ${emailPj}, empresa neste ato representada por ${nomeCliente}, brasileiro(a), Empresário(a)/Autônomo(a) com 
       inscrição no CPF nº ${cpf}, residente à ${cepDigitado.logradouro}, nº ${numeroResidencia}, bairro ${cepDigitado.bairro},
       CEP ${cepDigitado.cep}, na comarca de ${cepDigitado.localidade} - ${cepDigitado.uf}, telefone de contato ${telefone} e e-mail
       pessoal ${emailpessoal}, denominado(a) como <b>CONTRATANTE</b>
@@ -65,7 +41,6 @@ document.addEventListener("DOMContentLoaded", function () {
     </p>
   `;
 
-  
   
   document.getElementById("voltar").addEventListener("click", function () {
     window.location.href = "/index.html";
--- public/scriptColetorDeDados.js
+++ public/scriptColetorDeDados.js
@@ -1,10 +1,13 @@
 /* scriptColetorDeDados.js usado como motor para os arquivos STecSenai-lounge.html
 STecSenai-pickCliente.html, STecSenai-dadosContrato.html, STecSenai-localStorage.html
 STecSenai-contrato e STecSenai-consumir */
+
+// Valores declarados em função dos botões existentes na STecSenai-dadosContrato.html
 const botaoBuscarCadastro = document.querySelector("#botaoBuscarCadastro");
 const botaoGerarContrato = document.querySelector("#botaoGerarContrato");
 
 
+// Ação de coleta e armazenagem de dados da página STecSenai-dadosContrato.html
 document.getElementById('botaoImpressaoCnpj').addEventListener('click', async function() {
   const cepDigitado = document.getElementById('cep').value;
   const cnpjDigitado = document.getElementById('cnpj').value;
@@ -14,7 +17,15 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
   const telefone = document.getElementById('telefone').value;
   const email = document.getElementById('email').value;
   const servico = document.getElementById('servicos').value;
+  const testemunhaSTecSenai = document.getElementById('testemunhaSTecSenai');
 
+// Valida se nome, cpf e telefone estão preenchidos
+    if (!nomeCliente || !cpf || !telefone) {
+      alert("Preencha Nome, CPF e Telefone");
+      return; // Impede o envio dos dados
+    }
+  
+  
   try {
     let resCep = await fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`);
     let dataCep = await resCep.json();
@@ -25,18 +36,58 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
 
     let resCnpj = await fetch(`/cnpj/${cnpjDigitado}`);
     let dataCnpj = await resCnpj.json();
+  
+// Função para corrigir o nome fantasia, telefone Email e QSA da PJ caso não tenha -------------------------------------------------
 
-    // Preenchendo a tabela
-    document.getElementById('cnpj-td').textContent = cnpjDigitado;
+  function obterNomeFantasia() {
+    let fantasiaPj = dataCnpj.fantasia;
+      if (!fantasiaPj || fantasiaPj === "") {
+        fantasiaPj = "não atribuído";
+      } else {
+      fantasiaPj = dataCnpj.fantasia;
+    }
+     return fantasiaPj;} // Obtém o nome fantasia
+  let fantasiaPj = obterNomeFantasia();
+  
+  function obterTelefonePj () {
+    let telefonePj = dataCnpj.telefone;
+    if (!telefonePj || telefonePj === "") {
+      telefonePj = "não atribuído";
+    } else {telefonePj = dataCnpj.telefone;}
+     return telefonePj;}
+  let telefonePj = obterTelefonePj();
+  
+  function obterEmailPj () {
+    let emailPj = dataCnpj.email;
+    if (!emailPj || emailPj === "") {
+      emailPj = "não atribuído";
+    } else {emailPj = dataCnpj.email;}
+     return emailPj;}
+  let emailPj = obterEmailPj();
+  
+  function obterQsaPj () {
+    let socioPj = dataCnpj.qsa[0]
+    if (!socioPj || socioPj === "") {
+      socioPj = "não atribuído";
+    } else {socioPj = dataCnpj.qsa[0].nome;}
+     return socioPj;}
+  let socioPj = obterQsaPj();
+  
+  // ---------------------------------------- fantasiaPj telefonePj emailPj socioPj
+    
+
+// Preenchendo a tabela de verificação que aparecerá na página STecSenai-dadosContrato 
+    document.getElementById('cnpj-td').textContent = dataCnpj.cnpj;
+    document.getElementById('qsa-td').textContent = socioPj;
     document.getElementById('razao-social-td').textContent = dataCnpj.nome;
     document.getElementById('empresa-atividade-principal').textContent = dataCnpj.atividade_principal[0].text;
-    document.getElementById('empresa-nome-fantasia').textContent = dataCnpj.fantasia;
+    document.getElementById('empresa-nome-fantasia').textContent = fantasiaPj;
     document.getElementById('empresa-logradouro').textContent = dataCnpj.logradouro;
     document.getElementById('empresa-municipio').textContent = dataCnpj.municipio;
     document.getElementById('empresa-situacao').textContent = dataCnpj.situacao;
-    document.getElementById('telefone-td').textContent = dataCnpj.telefone;
+    document.getElementById('telefone-td').textContent = telefonePj;
     document.getElementById('endereco-td').textContent = `${dataCep.logradouro}, ${dataCep.bairro}, ${dataCep.localidade} - ${dataCep.uf}`;
-    document.getElementById('cep-td').textContent = cepDigitado;
+    document.getElementById('cep-td').textContent = dataCep.cep;
     document.getElementById('nome-cliente-td').textContent = nomeCliente;
     document.getElementById('cpf-td').textContent = cpf;
     document.getElementById('numero-residencia-td').textContent = numeroResidencia;
@@ -46,7 +97,7 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
 
     document.getElementById('data-table').style.display = 'block';
 
-    // Armazenar os dados no localStorage
+// Armazenar os dados no localStorage
     localStorage.setItem('dadosCnpj', JSON.stringify(dataCnpj));
     localStorage.setItem('cepDigitado', JSON.stringify(dataCep)); // Armazenar dados do CEP como JSON
     localStorage.setItem('nomeCliente', nomeCliente);
@@ -55,7 +106,11 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
     localStorage.setItem('telefone', telefone);
     localStorage.setItem('email', email);
     localStorage.setItem('servico', servico);
+    localStorage.setItem('fantasiaPj',fantasiaPj);
+    localStorage.setItem('telefonePj',telefonePj);
+    localStorage.setItem('emailPj',emailPj);
 
+  
   } catch (error) {
     console.error(error);
     alert("Erro ao buscar os dados. Por favor, verifique as informações digitadas e tente novamente.");
