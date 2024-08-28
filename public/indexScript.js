ea933a52e53bee99f68dbf17869eabaec9d6f11a
Glitch (backup-gerador-contratos-stec) <none>
2024-08-28 04:39:09 +0000

    🦛⛪ Updated with Glitch

--- public/indexScript.js
+++ public/indexScript.js
@@ -30,12 +30,12 @@ function carregarDados() {
       ).length;
 
       // Atualiza os elementos HTML ou armazena em variáveis conforme necessário
-      document.querySelector("#contadorNome4722").textContent = contador4722;
-      document.querySelector("#contadorNome4727").textContent = contador4727;
-      document.querySelector("#contadorNome4724").textContent = contador4724;
-      document.querySelector("#contadorNome4725").textContent = contador4725;
-      document.querySelector("#contadorNome4780").textContent = contador4780;
-      document.querySelector("#contadorNome4790").textContent = contador4790;
+      document.querySelector("#contadorNome4722").textContent = 40 - contador4722;
+      document.querySelector("#contadorNome4727").textContent = 1 - contador4727;
+      document.querySelector("#contadorNome4724").textContent = 10 - contador4724;
+      document.querySelector("#contadorNome4725").textContent = 0 - contador4725;
+      document.querySelector("#contadorNome4780").textContent = 10 - contador4780;
+      document.querySelector("#contadorNome4790").textContent = 10 - contador4790;
 
       // Armazena em variáveis para uso posterior
       // Exemplo:
@@ -49,38 +49,3 @@ function carregarDados() {
 
 // Chama a função para carregar os dados ao iniciar a página
 carregarDados();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* ---------------------------------------------------------------------
-function carregarDados() {
-  console.log("Carregando dados cadastrados...");
-
-  fetch("/buscarCadastroClientes")
-    .then((response) => response.json())
-    .then((data) => {
-      // Filtra os dados onde nascimentoPfSenaiST é igual a "4722"
-      const contadorNome = data.filter(
-        (item) => item.nascimentoPfSenaiST === "4722"
-      ).length;
-
-      // Atualiza o conteúdo do elemento HTML com o ID contadorNome
-      document.querySelector("#contadorNome").textContent = contadorNome;
-    })
-    .catch((error) => console.error("Erro ao carregar dados:", error));
-}
-
-// Chama a função para carregar os dados ao iniciar a página
-carregarDados();
------------------------------------------------------------------------ */
