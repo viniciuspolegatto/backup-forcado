db05d404fea1e664f78e8ec2551cd692e133b43d
viniciuspolegatto <93634376+viniciuspolegatto@users.noreply.github.com>
2025-01-20 11:59:42 -0300

    Create SOMA-detalhes.js

---
+++ public/SOMA-detalhes.js
@@ -0,0 +1,316 @@
+let botaoBuscarCadastro = document.querySelector("#botaoBuscarCadastro");
+let botaoGerarContrato = document.querySelector("#botaoGerarContrato");
+const urlParams = new URLSearchParams(window.location.search);
+const idProduto = urlParams.get('id'); // Obtém o ID do produto da URL
+const endpoint = 'https://raw.githubusercontent.com/viniciuspolegatto/apiCredenciamentoSomaSebraeSP/main/SebraeSpSomaConsultorias.json';
+const detalhesProduto = document.getElementById('detalhesProduto');
+const enviarPedidoBtn = document.getElementById('enviarPedido');
+const usarEmailWebBtn = document.getElementById('usarEmailWeb');
+const inputDataConsultoria = document.getElementById("dataConsultoria");
+
+document.addEventListener('DOMContentLoaded', () => {
+  carregarDetalhes();
+  configurarDataMinima(); // Chamada da função para configurar a data mínima
+});
+
+// Função para calcular 13 dias úteis
+function calcularDiasUteis(inicio, feriados, diasNecessarios) {
+  let contadorDiasUteis = 0;
+  const resultado = new Date(inicio);
+  while (contadorDiasUteis < diasNecessarios) {
+    resultado.setDate(resultado.getDate() + 1);
+    const diaSemana = resultado.getDay();
+    const dataFormatada = resultado.toISOString().split("T")[0];
+    if (
+      diaSemana !== 0 &&
+      diaSemana !== 6 &&
+      !feriados.includes(dataFormatada)
+    ) {
+      contadorDiasUteis++;
+    }
+  }
+  return resultado;
+}
+
+// Função para configurar a data mínima no campo de data
+function configurarDataMinima() {
+  const feriados = [
+    "2025-01-01",
+    "2025-04-21",
+    "2025-05-01",
+    "2025-09-07",
+    "2025-10-12",
+    "2025-11-02",
+    "2025-11-15",
+    "2025-12-25",
+  ];
+  const hoje = new Date();
+  const dataMinima = calcularDiasUteis(hoje, feriados, 13);
+  inputDataConsultoria.min = dataMinima.toISOString().split("T")[0];
+  inputDataConsultoria.disabled = false;
+}
+
+async function carregarDetalhes() {
+  try {
+    const res = await fetch(endpoint);
+    if (!res.ok) throw new Error('Erro ao buscar dados da API.');
+
+    const produtos = await res.json();
+    const produto = produtos.find(p => String(p.ID_Produto) === String(idProduto));
+
+    if (!produto) {
+      detalhesProduto.innerHTML = `<p>Produto não encontrado. Verifique o ID na URL.</p>`;
+      return;
+    }
+
+    // Exibe os detalhes do produto encontrado
+    detalhesProduto.innerHTML = `
+    <table style="width: 90%; text-align: justify;">
+      <thead>
+        <tr>
+          <th><h2>${produto.NomeProduto}</h2></th>
+        </tr>
+        <tr>
+          <th><h3>${produto.DescricaoProduto}</h3></th>
+        </tr>
+      </thead>
+    </table>
+    <p></P>
+    <table style="width: 90%; border-collapse: collapse; text-align: justify; border: 1px solid #ccc; margin: 5px;">
+      <tbody>
+        <tr>
+          <td><strong style="color: red;">PREÇO PARA O CLIENTE:</strong> ${Number(produto.Soma_Precificacao).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+          })}</td>
+          <td><strong>ID do Produto:</strong> ${produto.ID_Produto}</td>
+          <td><strong>Família:</strong> ${produto.Familia}</td>
+          <td><strong>Área:</strong> ${produto.Area}</td>
+        </tr>
+        <tr>
+          <td><strong>Pagamento ao Credenciado:</strong> ${Number(produto.Custo_Credenciado).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+          })}</td>
+          <td><strong>Natureza:</strong> ${produto.Natureza}</td>
+          <td><strong>Subárea:</strong> ${produto.Subarea}</td>
+          <td><strong>Público-alvo:</strong> ${produto.PublicoAlvo}</td>
+        </tr>
+        <tr>
+          <td><strong>Empresas Habilitadas:</strong> ${produto.EmpresasHabilitadas}</td>
+          <td><strong>Origem:</strong> ${produto.Origem}</td>
+          <td><strong>Carga horária:</strong> ${produto.CargaHoraria} horas</td>
+          <td><strong>Complexidade:</strong> ${produto.Complexidade}</td>
+        </tr>
+        <tr>
+          <td><strong>Modalidade:</strong> ${produto.Modalidade}</td>
+          <td><strong>Pago:</strong> ${produto.Pago}</td>
+        </tr>
+      </tbody>
+    </table>`;
+    
+  } catch (error) {
+    console.error('Erro ao carregar detalhes do produto:', error);
+    detalhesProduto.innerHTML = `<p>Erro ao carregar os detalhes do produto. Tente novamente mais tarde.</p>`;
+  }
+}
+
+// Ação de coleta e armazenagem de dados da página STecSenai-dadosContrato.html
+document.getElementById('botaoImpressaoCnpj').addEventListener('click', async function() {
+  let cepBruto = document.getElementById('cep').value;
+  let cnpjBruto = document.getElementById('cnpj').value;
+  let nomeCliente = document.getElementById('nomeClienteForm').value;
+  let nascimentoCliente = document.getElementById('nascimento').value;
+  let cpfBruto = document.getElementById('cpf').value;
+  let numeroResidencia = document.getElementById('numeroResidencia').value;
+  let telefone = document.getElementById('telefone').value;
+  let email = document.getElementById('email').value;
+  let testemunhaSelecionada = document.getElementById("listaDeTestemunhas");
+  let testemunhaDados = testemunhaSelecionada.value; // Obtém o valor selecionado
+
+  
+// Valida se nome, cpf e telefone estão preenchidos *******************************
+    if (!nomeCliente || !cpfBruto || !telefone) {
+      alert("Preencha Nome, CPF e Telefone");
+      return; // Impede o envio dos dados
+    }
+
+// Função para eliminar caracteres do CNPJ, CEP e CPF que não sáo números *********
+  function limparCnpj(cnpjBruto){
+    return cnpjBruto.replace(/\D/g, '');
+  } let cnpjDigitado = limparCnpj(cnpjBruto);
+  
+  function limparCep(cepBruto){
+    return cepBruto.replace(/\D/g, '');
+  } let cepDigitado = limparCep(cepBruto);
+  
+  function limparCpf(cpfBruto){
+    return cpfBruto.replace(/\D/g, '');
+  } let cpf = limparCpf(cpfBruto)
+
+  
+// SPLIT para Quebra a String PRODUTO e TESTEMUNHA para salvar detalhes no Banco de Dados **
+  const testemunhaDetalhes = testemunhaDados.split(" | "); // formato apresentado em STecSenai-dadosContrato.html
+  const testemunhaNome = testemunhaDetalhes[0];
+  const testemunhaCargo = testemunhaDetalhes[1];
+  const testemunhaCpf = testemunhaDetalhes[2];
+//------------------------------------------------- 
+
+
+
+// ELEMENTO PARA COLETAR INFORMAÇÕES DE CNPJ e CEP
+  try {
+    let resCep = await fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`);
+    let dataCep = await resCep.json();
+
+    if (dataCep.erro) {
+      throw new Error("CEP não encontrado");
+    }
+
+    let resCnpj = await fetch(`/cnpj/${cnpjDigitado}`);
+    let dataCnpj = await resCnpj.json();
+    console.log("retorno do script coletor de dados do dataCnpj" + dataCnpj)
+  
+// Função para corrigir o nome fantasia, telefone, Email, complemento e QSA da PJ caso não tenha -------------------------------------------------
+
+  function obterNomeFantasia() {
+    let fantasiaPj = dataCnpj.fantasia;
+      if (!fantasiaPj || fantasiaPj === "") {
+        fantasiaPj = "não atribuído";
+      } else {
+      fantasiaPj = dataCnpj.fantasia;
+    }
+     return fantasiaPj;} // Obtém o nome fantasia
+  let fantasiaPj = obterNomeFantasia();
+  
+  function obterTelefonePj () {
+    let telefonePj = dataCnpj.telefone;
+    if (!telefonePj || telefonePj === "") {
+      telefonePj = "não atribuído";
+    } else {telefonePj = dataCnpj.telefone;}
+     return telefonePj;}
+  let telefonePj = obterTelefonePj();
+  
+  function obterEmailPj () {
+    let emailPj = dataCnpj.email;
+    if (!emailPj || emailPj === "") {
+      emailPj = "não atribuído";
+    } else {emailPj = dataCnpj.email;}
+     return emailPj;}
+  let emailPj = obterEmailPj();
+  
+  function obterQsaPj () {
+    let socioPj = dataCnpj.qsa[0]
+    if (!socioPj || socioPj === "") {
+      socioPj = "não atribuído";
+    } else {socioPj = dataCnpj.qsa[0].nome;}
+     return socioPj;}
+  let socioPj = obterQsaPj();
+
+  function obterComplementoPj () {
+    let complementoPj = dataCnpj.complemento
+    if (!complementoPj || complementoPj === "") {
+      complementoPj = "";
+    } else {complementoPj = ", complemento "+dataCnpj.complemento;}
+     return complementoPj;}
+  let complementoPj = obterComplementoPj();
+
+
+
+// ----------------------------------------
+// Preenchendo a tabela de verificação que aparecerá na página STecSenai-dadosContrato 
+    document.getElementById('cnpj-td').textContent = dataCnpj.cnpj;
+    document.getElementById('qsa-td').textContent = socioPj;
+    document.getElementById('razao-social-td').textContent = dataCnpj.nome;
+    document.getElementById('empresa-atividade-principal').textContent = dataCnpj.atividade_principal[0].text;
+    document.getElementById('empresa-nome-fantasia').textContent = fantasiaPj;
+    document.getElementById('empresa-logradouro').textContent = dataCnpj.logradouro;
+    document.getElementById('empresa-municipio').textContent = dataCnpj.municipio;
+    document.getElementById('empresa-situacao').textContent = dataCnpj.situacao;
+    document.getElementById('empresa-porte').textContent = dataCnpj.porte;
+    document.getElementById('telefone-td').textContent = telefonePj;
+    document.getElementById('endereco-td').textContent = `${dataCep.logradouro}, ${dataCep.bairro}, ${dataCep.localidade} - ${dataCep.uf}`;
+    document.getElementById('cep-td').textContent = dataCep.cep;
+    document.getElementById('nome-cliente-td').textContent = nomeCliente;
+    document.getElementById('cpf-td').textContent = cpfBruto;
+    document.getElementById('numero-residencia-td').textContent = numeroResidencia;
+    document.getElementById('telefone-contato-td').textContent = telefone;
+    document.getElementById('email-td').textContent = email;
+    document.getElementById("testemunha-td").textContent = testemunhaNome;
+
+    document.getElementById('data-table').style.display = 'block';
+
+// Armazenar os dados no localStorage para ser usado na nova janela de contrato
+    localStorage.setItem('dadosCnpj', JSON.stringify(dataCnpj));
+    localStorage.setItem('cepDigitado', JSON.stringify(dataCep)); // Armazenar dados do CEP como JSON
+    localStorage.setItem('nomeCliente', nomeCliente);
+    localStorage.setItem('cpfBruto', cpfBruto);
+    localStorage.setItem('numeroResidencia', numeroResidencia);
+    localStorage.setItem('telefone', telefone);
+    localStorage.setItem('email', email);
+
+    localStorage.setItem('complementoPj', complementoPj);
+    localStorage.setItem('fantasiaPj',fantasiaPj);
+    localStorage.setItem('telefonePj',telefonePj);
+    localStorage.setItem('emailPj',emailPj);
+    
+    
+    localStorage.setItem('testemunhaNome', testemunhaNome);
+    localStorage.setItem('testemunhaCargo', testemunhaCargo);
+    localStorage.setItem('testemunhaCpf', testemunhaCpf);
+    
+
+
+
+/* ------------------------------------------------------ */ 
+
+    const data = {
+        info01: nomeCliente,
+        info02: cpf,
+        info03: nascimentoCliente,
+        info04: telefone,
+        info05: email,
+        info06: dataCep.cep,
+        info07: dataCep.logradouro,
+        info08: numeroResidencia,
+        info09: dataCep.bairro,
+        info10: dataCep.localidade,
+
+        info11: testemunhaNome,
+        info12: testemunhaCargo,
+        info13: testemunhaCpf,
+
+        info23: dataCnpj.cnpj,
+        info24: dataCnpj.nome,
+        info25: fantasiaPj,
+        info26: dataCnpj.atividade_principal[0].text,
+        info27: telefonePj,
+        info28: emailPj,
+        info29: socioPj,
+        info30: dataCnpj.situacao,
+        info31: dataCnpj.logradouro,
+        info32: dataCnpj.numero,
+        info33: complementoPj,
+        info34: dataCnpj.bairro,
+        info35: dataCnpj.municipio,
+        info37: dataCnpj.porte
+    };
+
+
+  
+  
+  } catch (error) {
+    console.error(error);
+    alert("Erro ao buscar os dados. Por favor, verifique as informações digitadas e tente novamente.");
+  }
+
+//-----------------------------------------------------------
+}); // FIM DO ADD EVENT LISTENER
+
+
+
+// Adiciona o evento de clique para o botão Gerar Contrato
+botaoGerarContrato.addEventListener("click", function() {
+    // Redireciona para a página de contrato
+    window.location.href = "STecSenai-contrato.html";
+});
