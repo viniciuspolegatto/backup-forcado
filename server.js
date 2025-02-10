62606b65c46ed7535a412b0455e6d54ffc96e201
Glitch (backup-gerador-contratos-stec) <none>
2025-02-10 12:05:32 +0000

    😬🌦️ Updated with Glitch

--- server.js
+++ server.js
@@ -1,30 +1,60 @@
-/* Arquivo server.js usado como motor para os arquivos STecSenai-lounge.html
-STecSenai-pickCliente.html, STecSenai-dadosContrato.html, STecSenai-localStorage.html
-STecSenai-contrato e STecSenai-consumir */
-
-const https = require('https');
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+/* Arquivo server.js usado como motor para acessar informações externas */
+const https = require("https");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 const app = express();
 const PORT = process.env.PORT || 3306;
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const session = require("express-session");
+
+dotenv.config();
 
 // Middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
 
+// Configuração de sessão
+app.use(
+  session({
+    secret: "segredo_super_secreto",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, httpOnly: true, maxAge: 3600000 }, // Expira em 1 hora
+  })
+);
 
 // ************************** Pool de Conexões MySQL *************************
 const db = mysql.createPool({
-  connectionLimit: 10, // Limite máximo de conexões
+  connectionLimit: 10, // Máximo de conexões simultâneas
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
-  database: process.env.MYSQL_DB
+  database: process.env.MYSQL_DB,
 });
+
+// Função para executar queries de forma segura
+function queryDatabase(sql, params, callback) {
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error("Erro ao obter conexão do pool:", err);
+      return callback(err, null);
+    }
+    connection.query(sql, params, (queryErr, results) => {
+      connection.release(); // Libera a conexão para reutilização
+      callback(queryErr, results);
+    });
+  });
+}
+
 
 // ************ Rota para busca de CNPJ na Receita Federal do Brasil ***************
 app.get('/cnpj/:cnpj', (req, res) => {
@@ -59,7 +89,7 @@ app.get('/', (req, res) => {
 app.post('/addData', (req, res) => {
   const { info01, info02, info03, info04, info05, info06, info07, info08, info09, info10, info11, info12, info13, info14, info15, info16, info17, info18, info19, info20, info21, info22, info23, info24, info25, info26, info27, info28, info29, info30, info31, info32, info33, info34, info35, info36, info37 } = req.body;
   const query = 'INSERT INTO ContratoSebraetecSenai (NomePfSenaiST, CpfPfSenaiST, nascimentoPfSenaiST, telefonePfSenaiST, emailPfSenaiST, cepPfSenaiST, logradouroPfSenaiST, numeroResidenciaPfSenaiST, bairroPfSenaiST, municipioPfSenaiST, testemunhaNomeSenaiST, testemunhaCargoSenaiST, testemunhaCpfSenaiST, ServFamiliaSenaiST, servTituloSenaiST, servRaeSenaiST, servRMSenaiST, servValorSenaiST, servTipoSenaiST, servPublicoSenaiST, servQhoraSenaiST, servModalidadeSenaiST, cnpjPj, razaoPj, fantasiaPj, atividadePj, telefonePj, emailPj, socioPj, situacaoPj, logradouroPj, numeroPj, complementoPj, bairroPj, municipioPj, solicitanteSenaiST, VAR1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
+  
   db.query(query, [info01, info02, info03, info04, info05, info06, info07, info08, info09, info10, info11, info12, info13, info14, info15, info16, info17, info18, info19, info20, info21, info22, info23, info24, info25, info26, info27, info28, info29, info30, info31, info32, info33, info34, info35, info36, info37], (err, result) => {
     if (err) {
       console.error('Erro ao inserir dados:', err);
@@ -75,7 +105,7 @@ app.post('/addData', (req, res) => {
 app.post('/addDataAgro', (req, res) => {
   const { info01, info02, info03, info04, info05, info06, info07, info08, info09, info10, info11, info12, info13, info14, info15, info16, info17, info18, info19, info20, info21, info22, info23, info24, info25, info26, info27, info28, info29, info30, info31, info32, info33, info34, info35, info36, info37 } = req.body;
   const query = 'INSERT INTO ContratoSebraetecAgro (NomePfAgroST, CpfPfAgroST, nascimentoPfAgroST, telefonePfAgroST, emailPfAgroST, cepPfAgroST, logradouroPfAgroST, numeroResidenciaPfAgroST, bairroPfAgroST, municipioPfAgroST, testemunhaNomeAgroST, testemunhaCargoAgroST, testemunhaCpfAgroST, servFamiliaAgroST, servTituloAgroST, servRaeAgroST, servRMAgroST, servValorAgroST, servTipoAgroST, servPublicoAgroST, servQhoraAgroST, servModalidadeAgroST, cnpjPjAgro, razaoPjAgro, fantasiaPjAgro, atividadePjAgro, telefonePjAgro, emailPjAgro, socioPjAgro, situacaoPjAgro, logradouroPjAgro, numeroPjAgro, complementoPjAgro, bairroPjAgro, municipioPjAgro, solicitanteAgroST, VAR1Agro) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
+  
   db.query(query, [info01, info02, info03, info04, info05, info06, info07, info08, info09, info10, info11, info12, info13, info14, info15, info16, info17, info18, info19, info20, info21, info22, info23, info24, info25, info26, info27, info28, info29, info30, info31, info32, info33, info34, info35, info36, info37], (err, result) => {
     if (err) {
       console.error('Erro ao inserir dados:', err);
@@ -91,7 +121,7 @@ app.post('/addDataAgro', (req, res) => {
 app.post('/addCliente', (req, res) => {
   const { info01, info02, info03, info04, info05, info06, info07, info08, info09, info10, info11, info12, info13, info14, info15, info16, info17, info18, info19, info20, info21, info22, info23, info24, info25, info26, info27, info28, info29, info30, info31, info32, info33, info34, info35, info36, info37, info38, info39 } = req.body;
   const query = 'INSERT INTO ClienteSebraetecSenai (ID_contrato, NomePfSenaiST, CpfPfSenaiST, nascimentoPfSenaiST, telefonePfSenaiST, emailPfSenaiST, cepPfSenaiST, logradouroPfSenaiST, numeroResidenciaPfSenaiST, bairroPfSenaiST, municipioPfSenaiST, testemunhaNomeSenaiST, testemunhaCargoSenaiST, testemunhaCpfSenaiST, ServFamiliaSenaiST, servTituloSenaiST, servRaeSenaiST, servRMSenaiST, servValorSenaiST, servTipoSenaiST, servPublicoSenaiST, servQhoraSenaiST, servModalidadeSenaiST, cnpjPj, razaoPj, fantasiaPj, atividadePj, telefonePj, emailPj, socioPj, situacaoPj, logradouroPj, numeroPj, complementoPj, bairroPj, municipioPj, procStarTec, numeroPasta, statusSTecSenai) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
+  
   db.query(query, [info01, info02, info03, info04, info05, info06, info07, info08, info09, info10, info11, info12, info13, info14, info15, info16, info17, info18, info19, info20, info21, info22, info23, info24, info25, info26, info27, info28, info29, info30, info31, info32, info33, info34, info35, info36, info37, info38, info39 ], (err, result) => {
     if (err) {
       console.error('Erro ao inserir dados:', err);
@@ -107,7 +137,7 @@ app.post('/addCliente', (req, res) => {
 app.post('/addClienteAgro', (req, res) => {
   const { info01, info02, info03, info04, info05, info06, info07, info08, info09, info10, info11, info12, info13, info14, info15, info16, info17, info18, info19, info20, info21, info22, info23, info24, info25, info26, info27, info28, info29, info30, info31, info32, info33, info34, info35, info36, info37, info38, info39 } = req.body;
   const query = 'INSERT INTO ClienteSebraetecAgro (ID_contratoAgro, NomePfAgroST, CpfPfAgroST, nascimentoPfAgroST, telefonePfAgroST, emailPfAgroST, cepPfAgroST, logradouroPfAgroST, numeroResidenciaPfAgroST, bairroPfAgroST, municipioPfAgroST, testemunhaNomeAgroST, testemunhaCargoAgroST, testemunhaCpfAgroST, servFamiliaAgroST, servTituloAgroST, servRaeAgroST, servRMAgroST, servValorAgroST, servTipoAgroST, servPublicoAgroST, servQhoraAgroST, servModalidadeAgroST, cnpjPjAgro, razaoPjAgro, fantasiaPjAgro, atividadePjAgro, telefonePjAgro, emailPjAgro, socioPjAgro, situacaoPjAgro, logradouroPjAgro, numeroPjAgro, complementoPjAgro, bairroPjAgro, municipioPjAgro, procStarTecAgro, numeroPastaAgro, statusSTecAgro) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
+  
   db.query(query, [info01, info02, info03, info04, info05, info06, info07, info08, info09, info10, info11, info12, info13, info14, info15, info16, info17, info18, info19, info20, info21, info22, info23, info24, info25, info26, info27, info28, info29, info30, info31, info32, info33, info34, info35, info36, info37, info38, info39 ], (err, result) => {
     if (err) {
       console.error('Erro ao inserir dados:', err);
@@ -122,7 +152,7 @@ app.post('/addClienteAgro', (req, res) => {
 // ************************** Rota para buscar todos os cadastros para CONTRATO SENAI ***************
 app.get('/buscarContrato', (req, res) => {
   const query = 'SELECT ID_Contrato, NomePfSenaiST, CpfPfSenaiST, municipioPfSenaiST, telefonePfSenaiST, razaoPj, fantasiaPj, municipioPj, telefonePj, emailPfSenaiST FROM ContratoSebraetecSenai';
-
+  
   db.query(query, (err, results) => {
     if (err) {
       console.error('Erro ao buscar dados:', err);
@@ -137,7 +167,7 @@ app.get('/buscarContrato', (req, res) => {
 // ************************** Rota para buscar todos os cadastros para CONTRATO AGRO ***************
 app.get('/buscarContratoAgro', (req, res) => {
   const query = 'SELECT ID_ContratoAgro, NomePfAgroST, CpfPfAgroST, municipioPfAgroST, telefonePfAgroST, razaoPjAgro, fantasiaPjAgro, municipioPjAgro, telefonePjAgro, emailPfSenaiST FROM ContratoSebraetecAgro';
-
+  
   db.query(query, (err, results) => {
     if (err) {
       console.error('Erro ao buscar dados:', err);
@@ -152,7 +182,7 @@ app.get('/buscarContratoAgro', (req, res) => {
 // ************************** Rota para buscar todos os CLIENTES CONSUMIDORES SEBRAETEC SENAI***************
 app.get('/buscarCadastroClientes', (req, res) => {
   const query = 'SELECT ID, NomePfSenaiST, CpfPfSenaiST, telefonePfSenaiST, emailPfSenaiST, municipioPfSenaiST, testemunhaNomeSenaiST, servTituloSenaiST, servRaeSenaiST, servRMSenaiST, servValorSenaiST, servTipoSenaiST, servQhoraSenaiST, cnpjPj, razaoPj, fantasiaPj, municipioPj, telefonePj, emailPfSenaiST, procStarTec, numeroPasta, statusSTecSenai FROM ClienteSebraetecSenai';
-
+  
   db.query(query, (err, results) => {
     if (err) {
       console.error('Erro ao buscar dados:', err);
@@ -167,7 +197,7 @@ app.get('/buscarCadastroClientes', (req, res) => {
 // ************************** Rota para buscar todos os CLIENTES CONSUMIDORES SEBRAETEC AGRO***************
 app.get('/buscarCadastroClientesAgro', (req, res) => {
   const query = 'SELECT ID_Agro, NomePfAgroST, CpfPfAgroST, telefonePfAgroST, emailPfAgroST, municipioPfAgroST, testemunhaNomeAgroST, servTituloAgroST, servRaeAgroST, servRMAgroST, servValorAgroST, servTipoAgroST, servQhoraAgroST, cnpjPjAgro, razaoPjAgro, fantasiaPjAgro, municipioPjAgro, telefonePjAgro, emailPfAgroST, procStarTecAgro, numeroPastaAgro, statusSTecAgro FROM ClienteSebraetecAgro';
-
+  
   db.query(query, (err, results) => {
     if (err) {
       console.error('Erro ao buscar dados:', err);
@@ -188,7 +218,7 @@ app.get('/buscarPorNome/:nomeCliente', (req, res) => {
 
   // Adiciona % antes e depois do nomeCliente para busca parcial
   const searchParam = `%${nomeCliente}%`;
-
+  
   db.query(query, [searchParam], (err, results) => {
     if (err) {
       console.error('Erro ao buscar dados:', err);
@@ -206,7 +236,7 @@ app.get('/buscarPorNome/:nomeCliente', (req, res) => {
 app.get('/buscarPorIdContrato/:idContrato', (req, res) => {
   const idContrato = req.params.idContrato;
   const query = 'SELECT * FROM ContratoSebraetecSenai WHERE ID_Contrato = ?';
-
+  
   db.query(query, [idContrato], (err, results) => {
     if (err) {
       console.error('Erro ao buscar dados:', err);
@@ -255,7 +285,7 @@ app.put('/atualizarCliente', (req, res) => {
 
   // Adiciona o idCliente no array de valores
   updateValues.push(idCliente);
-
+  
   // Executa a query com os campos e valores dinâmicos
   db.query(query, updateValues, (err, result) => {
     if (err) {
@@ -269,9 +299,10 @@ app.put('/atualizarCliente', (req, res) => {
 });
 
 
-/* / ************** ESTRUTURA DE ENVIO DE E-MAILS DIRETO DO BACKEND ***************
-
-app.post('/enviarEmail', async (req, res) => {
+//================================================================================
+// ************** ESTRUTURA DE ENVIO DE E-MAILS DIRETO DO BACKEND ***************
+//=================================================================================
+/* app.post('/enviarEmail', async (req, res) => {
     const { emailBody, solicitanteEmail } = req.body;
 
     console.log('Corpo do e-mail recebido:', emailBody); // Log para depuração
@@ -300,8 +331,74 @@ app.post('/enviarEmail', async (req, res) => {
         res.status(500).send('Erro ao enviar o e-mail.');
     }
 });
+//=================================================================================== */
 
-*/
+
+
+// ***************** CADASTRO E LOGIN DE USUÁRIOS NO PORTAL *************************
+// ROTA PARA CADASTRO DE NOVOS USUÁRIOS AUTORIZADOS A TRABALHAR NO PORTAL
+
+app.post('/register', (req, res) => {
+  const { info01, info02, info03 } = req.body;
+  const query = 'INSERT INTO USER_SENHAS_EMAIL (email, password, elemento) VALUES (?, ?, ?)';
+  
+  db.query(query, [info01, info02, info03], (err, result) => {
+    if (err) {
+      console.error('Erro ao inserir dados:', err);
+      res.status(500).send('Erro ao inserir dados: ' + err.message);
+      return;
+    }
+    console.log('Dados inseridos com sucesso:', result);
+    res.send('Dados adicionados ao banco de dados');
+  });   
+});
+
+
+
+// ***************** ROTA PARA LOGIN **************************
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  queryDatabase(
+    "SELECT * FROM USER_SENHAS_EMAIL WHERE email = ? AND password = ?",
+    [username, password],
+    (err, results) => {
+      if (err) {
+        console.error("Erro no banco de dados:", err);
+        return res.status(500).json({ message: "Erro no servidor" });
+      }
+
+      if (results.length > 0) {
+        req.session.user = username; // Armazena o usuário na sessão
+        res.json({ success: true, message: "Login bem-sucedido" });
+      } else {
+        res.status(401).json({ success: false, message: "Usuário ou senha incorretos" });
+      }
+    }
+  );
+});
+
+// Rota para verificar autenticação
+app.get("/auth", (req, res) => {
+  if (req.session.user) {
+    res.json({ authenticated: true, user: req.session.user });
+  } else {
+    res.status(401).json({ authenticated: false });
+  }
+});
+
+// Rota de logout
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ success: false, message: "Erro ao realizar logout" });
+    }
+    res.clearCookie("connect.sid"); // Remove o cookie de sessão
+    res.json({ success: true, message: "Logout realizado com sucesso" });
+  });
+});
+
+
 
 // ************************** Inicialização do Servidor *************************
 app.listen(PORT, () => {
